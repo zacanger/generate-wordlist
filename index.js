d2de6fd97dc4a19c22e3a06077e5bb24a9956eea
zacanger <zac@zacanger.com>
2020-03-25 15:32:14 -0600

    chore: switch to eslint

--- index.js
+++ index.js
@@ -11,15 +11,21 @@ if (module.parent || !args[0] || !args[1]) {
   process.exit(0)
 }
 
-const pReg = args[2] && args[2] === '--no-punc'
-  ? /[.,\/#!$%\^&\*;:{}\[\]=\-_`~()]/g
-  : ''
+const pReg =
+  args[2] && args[2] === '--no-punc' ? /[!#$%&()*,./:;=[\]^_`{}~\-]/g : ''
 
 const pRep = pReg ? ' ' : ''
 
-fs.writeFileSync(args[1],
-  [...new Set(fs.readFileSync(path.resolve(args[0]))
-    .toString()
-    .replace(pReg, pRep)
-    .split(/\s/)
-    .sort())].join('\n') + '\n')
+fs.writeFileSync(
+  args[1],
+  [
+    ...new Set(
+      fs
+        .readFileSync(path.resolve(args[0]))
+        .toString()
+        .replace(pReg, pRep)
+        .split(/\s/)
+        .sort()
+    ),
+  ].join('\n') + '\n'
+)
